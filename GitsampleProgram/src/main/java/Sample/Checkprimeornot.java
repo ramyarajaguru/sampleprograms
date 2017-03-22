917f473db8d139e950cda7a0792b5881b6b8ff32
ramyarajaguru <anuragu25@gmail.com>
2017-03-22 18:48:13 +0530

    update prime number program

---
+++ GitsampleProgram/src/main/java/Sample/Checkprimeornot.java
@@ -0,0 +1,27 @@
+package Sample;
+
+public class Checkprimeornot {
+
+	public static void main(String[] args) {
+	
+		
+		Checkprimeornot cp=new Checkprimeornot();
+		cp.checkPrime(70);
+	}
+		
+		public void checkPrime(int n)
+		{
+		int count=0;
+		for(int i=1; i<=n; i++)
+		{
+			if(n%i==0)
+				count++;
+		}
+		if(count==2)
+			System.out.println(" The given "+n+" number is a prime number");
+		else
+			System.out.println(" The given "+n+" number is not a prime number");
+
+	}
+
+}
